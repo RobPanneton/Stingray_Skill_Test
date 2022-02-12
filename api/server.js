0fc99136b6d8e8d1ff16eb935b6016d01adc9b5c
RobPanneton <robert.panneton@gmail.com>
2022-02-12 11:03:50 -0500

    add fallback endpoint

--- api/server.js
+++ api/server.js
@@ -10,6 +10,13 @@ app.get("/api/swimlanes", async (req, res) => {
   res.json(swimlaneData);
 });
 
+app.get("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: "This is not what you were looking for.",
+  });
+});
+
 // Start the server
 app.listen(3100, () => {
   console.log(`Starting the api on port 3100`);
